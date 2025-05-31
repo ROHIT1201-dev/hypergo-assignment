e243eafaab8c23d4b022a32a14dc164c040a746d
Rohit raj <rohit1201.be21@chitkara.edu.in>
2025-05-31 20:13:33 +0530

    Addded redis password and port to env

--- config/redisClient.js
+++ config/redisClient.js
@@ -3,8 +3,8 @@ import Redis from 'ioredis'
 
 const redis = new Redis({
   host: 'redis-15345.crce182.ap-south-1-1.ec2.redns.redis-cloud.com',
-  port: 15345,
-  password: 'YBJREpxw46kio9jZkpd4IkvejPapVZNg',
+  port: process.env.REDIS_PORT,
+  password:process.env.REDIS_PASS ,
 
 });
 
